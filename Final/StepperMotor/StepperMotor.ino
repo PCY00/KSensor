357c78df05701049b168530ae76ee2fb3f15c5eb
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-03-01 23:05:13 +0900

    Update StepperMotor.ino

--- Final/StepperMotor/StepperMotor.ino
+++ Final/StepperMotor/StepperMotor.ino
@@ -1,7 +1,8 @@
-#include <Arduino.h>
-#include "AccelStepper.h"
+  #include <Arduino.h>
+  #include <AccelStepper.h>
+  #define motorInterfaceType 1
 
-#define motorInterfaceType 1
+#define BOTTOM_SPEED 100
 
 const int dirPin_Top_1 = 10;
 const int stepPin_Top_1 = 11;
@@ -9,8 +10,10 @@ const int stepPin_Top_1 = 11;
 const int dirPin_Top_2 = 12;
 const int stepPin_Top_2 = 13;
 
+
 const int dirPin_Top_3 = 2;
 const int stepPin_Top_3 = 3;
+
 
 const int dirPin_Bottom_1 = 4;
 const int stepPin_Bottom_1 = 5;
@@ -21,9 +24,11 @@ const int stepPin_Bottom_2 = 7;
 const int dirPin_Bottom_3 = 8;
 const int stepPin_Bottom_3 = 9;
 
+
 AccelStepper stepper_Top_1(motorInterfaceType, stepPin_Top_1, dirPin_Top_1);
 AccelStepper stepper_Top_2(motorInterfaceType, stepPin_Top_2, dirPin_Top_2);
 AccelStepper stepper_Top_3(motorInterfaceType, stepPin_Top_3, dirPin_Top_3);
+
 
 AccelStepper stepper_Bottom_1(motorInterfaceType, stepPin_Bottom_1, dirPin_Bottom_1);
 AccelStepper stepper_Bottom_2(motorInterfaceType, stepPin_Bottom_2, dirPin_Bottom_2);
@@ -32,6 +37,7 @@ AccelStepper stepper_Bottom_3(motorInterfaceType, stepPin_Bottom_3, dirPin_Botto
 void setup()
 {
   // put your setup code here, to run once:
+  
   stepper_Top_1.setMaxSpeed(1000.0);
   stepper_Top_1.setAcceleration(20.0);
   stepper_Top_1.moveTo(55000);
@@ -46,21 +52,22 @@ void setup()
   stepper_Top_3.setAcceleration(20.0);
   stepper_Top_3.moveTo(55000);
   stepper_Top_3.setSpeed(1000);
-
-  stepper_Bottom_1.setMaxSpeed(60.0);
-  stepper_Bottom_1.setAcceleration(20.0);
-  stepper_Bottom_1.moveTo(1000);
-  stepper_Bottom_1.setSpeed(60);
   
-  stepper_Bottom_2.setMaxSpeed(60.0);
-  stepper_Bottom_2.setAcceleration(20.0);
-  stepper_Bottom_2.moveTo(1000);
-  stepper_Bottom_2.setSpeed(60);
 
-  stepper_Bottom_3.setMaxSpeed(60.0);
-  stepper_Bottom_3.setAcceleration(20.0);
-  stepper_Bottom_3.moveTo(1000);
-  stepper_Bottom_3.setSpeed(60);
+  stepper_Bottom_1.setMaxSpeed(BOTTOM_SPEED);
+  stepper_Bottom_1.setAcceleration(10.0);
+  stepper_Bottom_1.moveTo(5000);
+  stepper_Bottom_1.setSpeed(BOTTOM_SPEED);
+  
+  stepper_Bottom_2.setMaxSpeed(BOTTOM_SPEED);
+  stepper_Bottom_2.setAcceleration(10.0);
+  stepper_Bottom_2.moveTo(5000);
+  stepper_Bottom_2.setSpeed(BOTTOM_SPEED);
+
+  stepper_Bottom_3.setMaxSpeed(BOTTOM_SPEED);
+  stepper_Bottom_3.setAcceleration(10.0);
+  stepper_Bottom_3.moveTo(5000);
+  stepper_Bottom_3.setSpeed(BOTTOM_SPEED);
 }
 
 void loop()
@@ -79,6 +86,7 @@ void loop()
   {
     stepper_Top_3.moveTo(-stepper_Top_3.currentPosition());
   }
+  
   
   
   if (stepper_Bottom_1.distanceToGo() == 0)
