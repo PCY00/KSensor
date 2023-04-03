1f20384717f64cefdd1cf4aa9b0a40eff9bb19b8
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-04-04 00:19:54 +0900

    Create Tx3.ino

---
+++ Try/Using_LoRa/FeatherM0_LoRa/5sec_without duplication/Tx3.ino
@@ -0,0 +1,87 @@
+#include <RH_RF95.h>
+#include <SPI.h>
+#include <PMS.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+#define RF95_FREQ_1 920.9
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+PMS pms(Serial1);
+PMS::DATA data;
+
+uint8_t msg[2] = {0,};
+boolean post = false;
+
+void dataProcessing(int16_t pm2_5data);
+
+void setup() {
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  
+  Serial.begin(115200);
+  Serial1.begin(9600);
+
+  pms.passiveMode();
+//pms.activeMode();  
+  
+  while(!Serial){
+    delay(1);
+  }
+  delay(100);
+
+  Serial.println("Tx3 test");
+  
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+  
+  // 첫 번째 드라이버 설정
+  while(!rf95.init()){
+    Serial.println("LoRa radio init failed");
+    while(1);
+  }
+  Serial.println("LoRa radio init success");
+  
+  if (!rf95.setFrequency(RF95_FREQ_1)) {
+    Serial.println("setFrequency failed");  
+    while(1);
+  }
+  Serial.print("Using F"); Serial.println(RF95_FREQ_1);
+  
+  rf95.setTxPower(23, false);
+}
+
+
+void loop(){
+  if(rf95.available()){
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if(rf95.recv(buf, &len)){
+      if(buf[0] == 0x03){
+        post = true;
+      }
+    }
+  }
+  
+  if(post = true){
+    pms.requestRead();
+    
+    if(pms.readUntil(data)){
+      int16_t pm2_5 = data.PM_AE_UG_2_5;
+      dataProcessing(pm2_5);
+         
+      rf95.send(msg, 2);
+      rf95.waitPacketSent();
+    }
+  }
+  post = false;
+}
+
+void dataProcessing(int16_t pm2_5data){
+  msg[0] = pm2_5data & 0xFF;
+  msg[1] = (0xff & (pm2_5data >> 8));
+}
