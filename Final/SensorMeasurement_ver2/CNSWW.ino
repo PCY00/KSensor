443ece9b2b3db998291d173b47bfbc5dd50af169
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-05-03 12:24:54 +0900

    Create CNSWW.ino

---
+++ Final/SensorMeasurement_ver2/CNSWW.ino
@@ -0,0 +1,122 @@
+//so2, no2, co
+#define ContinueMode 0
+#define PollingMode 1
+
+char CO_data[60];    //Hardware-designed co data archiving
+char NO2_data[60];    //Hardware-designed no2 data archiving
+char SO2_data[60];    //Hardware-designed so2 data archiving
+float direct[] = {3.84,1.98,2.25,0.41,0.45,0.32,0.90,0.62,1.40,1.19,3.08,2.93,4.62,4.04,4.33,3.43};
+
+//function
+bool runEvery(unsigned long interval);
+float value_convert(String value);
+
+void setup(){
+  Serial.begin(9600);
+  Serial3.begin(9600);        //SO2
+  Serial2.begin(9600);        //NO2
+  Serial1.begin(9600);        //CO
+  delay(1000);
+}
+
+void loop(){
+// wind speed 
+  int wind_speed_val = analogRead(A1);
+  float wind_speed_outvoltage = wind_speed_val * (5.0 / 1023.0);
+  int wind_speed_print = 6 * wind_speed_outvoltage;
+// wind direct
+  int wind_direction_val = analogRead(A0);
+  float wind_direction_outvoltage = wind_direction_val * 5 / 1023.0;
+
+  if(runEvery(5000)){
+    //SO2, NO2, CO
+    #if PollingMode
+      Serial1.write('\r');
+      Serial2.write('\r');
+      Serial3.write('\r');
+      delay(1000);
+    #else
+      delay(100);
+    #endif
+    
+    int i = 0;
+
+    //SO2, NO2 data get
+    while (Serial1.available()) { // read from AGSM port, send to Serial port to interupt continuous output send 'c''/r' without line ending, may have to send more than once.
+      CO_data[i++] = Serial1.read();
+    }
+    delayMicroseconds(10);
+    i = 0;
+    while (Serial2.available()) {
+      NO2_data[i] = Serial2.read();
+      i++;
+    }
+    delayMicroseconds(10);
+    i = 0;
+    while (Serial3.available()) {
+      SO2_data[i] = Serial3.read();
+      i++;
+    }
+    delay(100);
+    
+    String str_co_data = "";
+    String str_no2_data = "";
+    String str_so2_data = "";
+
+    for(int j = 12; j < 18; j++){
+       str_co_data += CO_data[j];
+       str_no2_data += NO2_data[j];
+       str_so2_data += SO2_data[j];
+    }
+
+    float coPPM, no2PPM, so2PPM;
+  
+    coPPM = value_convert(str_co_data);
+    no2PPM = value_convert(str_no2_data);
+    so2PPM = value_convert(str_so2_data);
+
+    // wind degree get
+    float degree;
+    for(int j =0; j < 16; j++){
+      if(direct[j]-0.02 < wind_direction_outvoltage && direct[j]+0.02 > wind_direction_outvoltage){
+         degree = j * 22.5;
+         break;
+      }
+    }
+
+
+    Serial.print(coPPM, 3);
+    Serial.print(" ");
+    Serial.print(no2PPM, 3);
+    Serial.print(" ");
+    Serial.print(so2PPM, 3);
+    Serial.print(" ");
+    Serial.print(degree);
+    Serial.print(" ");
+    Serial.println(wind_speed_print);
+  }
+}
+
+bool runEvery(unsigned long interval){
+  static unsigned long previousMillis = 0;
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    return true;
+  }
+  return false;
+}
+
+float value_convert(String value){
+  int start = value.indexOf(',') + 1; // 시작 인덱스
+  int end = value.indexOf(',', start); // 끝 인덱스
+
+  float con_ppm = 0.0;
+  // 쉼표와 쉼표 사이의 값
+  String con_value = value.substring(start, end);
+  int convalue = con_value.toInt();
+  con_ppm = (float)convalue / 1000;
+  
+  return con_ppm;
+}
