b773f49df4c70661695457a1191471809688e6cd
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-05-20 10:00:15 +0900

    Update StepperMotor.ino

--- Final/StepperMotor/StepperMotor.ino
+++ Final/StepperMotor/StepperMotor.ino
@@ -4,25 +4,25 @@
 
 #define BOTTOM_SPEED 100
 
-const int dirPin_Top_1 = 10;
-const int stepPin_Top_1 = 11;
+const int dirPin_Top_1 = 4;
+const int stepPin_Top_1 = 5;
 
-const int dirPin_Top_2 = 12;
-const int stepPin_Top_2 = 13;
-
-
-const int dirPin_Top_3 = 2;
-const int stepPin_Top_3 = 3;
+const int dirPin_Top_2 = 8;
+const int stepPin_Top_2 = 9;
 
 
-const int dirPin_Bottom_1 = 4;
-const int stepPin_Bottom_1 = 5;
+const int dirPin_Top_3 = 12;
+const int stepPin_Top_3 = 13;
+
+
+const int dirPin_Bottom_1 = 2;
+const int stepPin_Bottom_1 = 3;
 
 const int dirPin_Bottom_2 = 6;
 const int stepPin_Bottom_2 = 7;
 
-const int dirPin_Bottom_3 = 8;
-const int stepPin_Bottom_3 = 9;
+const int dirPin_Bottom_3 = 10;
+const int stepPin_Bottom_3 = 11;
 
 
 AccelStepper stepper_Top_1(motorInterfaceType, stepPin_Top_1, dirPin_Top_1);
@@ -38,20 +38,20 @@ void setup()
 {
   // put your setup code here, to run once:
   
-  stepper_Top_1.setMaxSpeed(1000.0);
+  stepper_Top_1.setMaxSpeed(500.0);
   stepper_Top_1.setAcceleration(20.0);
   stepper_Top_1.moveTo(55000);
-  stepper_Top_1.setSpeed(1000);
+  stepper_Top_1.setSpeed(500);
 
-  stepper_Top_2.setMaxSpeed(1000.0);
+  stepper_Top_2.setMaxSpeed(500.0);
   stepper_Top_2.setAcceleration(20.0);
   stepper_Top_2.moveTo(55000);
-  stepper_Top_2.setSpeed(1000);
+  stepper_Top_2.setSpeed(500);
 
-  stepper_Top_3.setMaxSpeed(1000.0);
+  stepper_Top_3.setMaxSpeed(500.0);
   stepper_Top_3.setAcceleration(20.0);
   stepper_Top_3.moveTo(55000);
-  stepper_Top_3.setSpeed(1000);
+  stepper_Top_3.setSpeed(500);
   
 
   stepper_Bottom_1.setMaxSpeed(BOTTOM_SPEED);
