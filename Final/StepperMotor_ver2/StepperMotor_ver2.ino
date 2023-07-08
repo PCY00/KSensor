1139a267c45da44562e9aa767be556481dafd40c
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-07-08 14:00:39 +0900

    Update StepperMotor_ver2.ino

--- Final/StepperMotor_ver2/StepperMotor_ver2.ino
+++ Final/StepperMotor_ver2/StepperMotor_ver2.ino
@@ -23,35 +23,61 @@ void setup(){
   pinMode(step_3, OUTPUT);
   pinMode(dir_3, OUTPUT);
 
+
   digitalWrite(en_1, LOW);
   digitalWrite(step_1, LOW);
-  digitalWrite(dir_1, LOW);
 
   digitalWrite(en_2, LOW);
   digitalWrite(step_2, LOW);
-  digitalWrite(dir_2, LOW);
 
   digitalWrite(en_3, LOW);
   digitalWrite(step_3, LOW);
-  digitalWrite(dir_3, LOW);
-  
+
   delay(1000);
-  
 }
 
 void loop(){
-  digitalWrite(step_1, HIGH);
-  delay(1);
-  digitalWrite(step_1, LOW);
-  delay(1);
+  digitalWrite(dir_1, LOW);
+  digitalWrite(dir_2, LOW);
+  digitalWrite(dir_3, LOW);
+  for(uint16_t i = 0; i < 1600; i++){
+    //1
+    digitalWrite(step_1, HIGH);
+    delay(1);
+    digitalWrite(step_1, LOW);
+    delay(1);
+    //2
+    digitalWrite(step_2, HIGH);
+    delay(1);
+    digitalWrite(step_2, LOW);
+    delay(1);
+    //3
+    digitalWrite(step_3, HIGH);
+    delay(1);
+    digitalWrite(step_3, LOW);
+    delay(1);
+  }
+  delay(1000);
 
-  digitalWrite(step_2, HIGH);
-  delay(1);
-  digitalWrite(step_2, LOW);
-  delay(1);
-
-  digitalWrite(step_3, HIGH);
-  delay(1);
-  digitalWrite(step_3, LOW);
-  delay(1);
+  digitalWrite(dir_1, HIGH);
+  digitalWrite(dir_2, HIGH);
+  digitalWrite(dir_3, HIGH);
+  for(uint16_t i = 0; i < 1600; i++){
+    //1
+    digitalWrite(step_1, HIGH);
+    delay(1);
+    digitalWrite(step_1, LOW);
+    delay(1);
+    //2
+    digitalWrite(step_2, HIGH);
+    delay(1);
+    digitalWrite(step_2, LOW);
+    delay(1);
+    //3
+    digitalWrite(step_3, HIGH);
+    delay(1);
+    digitalWrite(step_3, LOW);
+    delay(1);
+  }
+  delay(1000);
 }
