64fda1d571d45492014a17ba197abf482b86c324
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-07-17 00:35:08 +0900

    Create APM.h

---
+++ Make_Stepper/ver3/APM.h
@@ -0,0 +1,32 @@
+//tmc2209 library
+
+#ifndef _APM_H_
+#define _APM_H_
+
+#include <Arduino.h>
+
+class APM
+{
+    public:
+        APM(int stepPin, int dirPin, int enPin);
+        APM(int stepPin, int dirPin, int enPin, int swich);
+        void motorTopSetup();
+        void setting(int speeds, int rounds);
+        void run();
+
+    private:
+        int stepPin;
+        int dirPin;
+        int enPin;
+
+        int swich;
+
+        //setting
+        int speeds;
+        int rounds;
+
+        static int step;
+};
+
+
+#endif
