be139394736dfef00c8e6c061a9584f37738639a
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-08-19 12:49:17 +0900

    Update C_3.ino

--- Make_Stepper/ver5_mobius/C_3.ino
+++ Make_Stepper/ver5_mobius/C_3.ino
@@ -1,37 +1,36 @@
 #include "m_3.h"
 
-m_3 bottom(2, 3, 4);
-m_3 top_1(5, 6, 7, 22);
-m_3 top_2(8, 9, 10, 24);
-m_3 top_3(11, 12, 13, 26);
+//m_3 bottom(2, 3, 4);
+m_3 top_1(5, 6, 7, 26);
+//m_3 top_2(8, 9, 10, 22);
+//m_3 top_3(11, 12, 13, 24);
 
 void setup() {
   // put your setup code here, to run once:
-  bottom.setRound(7200);
+  //bottom.setRound(7200);
   top_1.setRound(7200);
-  top_2.setRound(7200);
-  top_3.setRound(7200);
+  //top_2.setRound(7200);
+  //top_3.setRound(7200);
 
   //10ms down
-  bottom.setspeed(10);
+  //bottom.setspeed(10);
   top_1.setspeed(1);
-  top_2.setspeed(1);
-  top_3.setspeed(1);
+  //top_2.setspeed(1);
+  //top_3.setspeed(1);
 
   //top motor setup
   top_1.TopSetup();
-  top_2.TopSetup();
-  top_3.TopSetup();
+  //top_2.TopSetup();
+  //top_3.TopSetup();
 
 }
 
 void loop() {
-  if(Serial.available() > 0){
+  /*if(Serial.available() > 0){
     int inputValue = Serial.parseInt();
-    
-  }
-  bottom.moveto();
+  }*/
+  //bottom.moveto();
   top_1.moveto();
-  top_2.moveto();
-  top_3.moveto();
+  //top_2.moveto();
+  //top_3.moveto();
 }
