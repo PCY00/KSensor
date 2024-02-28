b75777f0c0aa76df781ee52baa13d57aecf87bb4
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-02-28 19:16:31 +0900

    Add files via upload

---
+++ 2024_02/APM-2_code/RPM/RPM_V1/RPM_v1.h
@@ -0,0 +1,32 @@
+#ifndef RPM_V1_H
+#define RPM_V1_H
+#include <SoftwareSerial.h>
+
+#define baudRate 19200
+#define stx 0x02
+#define Checksum 0xFF
+
+//request
+#define request_cmd 0x10
+#define request_size_1 0x00
+#define request_size_2 0x00
+
+//response
+#define response_cmd 0x11
+#define response_size_1 0x18
+#define response_size_2 0x00
+
+class RPM_V1 {
+public:
+	RPM_V1(uint8_t rx, uint8_t tx);
+	void Value_Query();
+	int value_Return();
+	String show_data();
+	bool runEvery(unsigned long interval);
+
+private:
+  SoftwareSerial rpm;
+	uint16_t Data[12];
+};
+
+#endif
