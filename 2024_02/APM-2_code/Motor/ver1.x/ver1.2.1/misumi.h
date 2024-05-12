9e91766f27fa0a005ff6296d58e9f0d9eef9738d
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-05-12 18:09:28 +0900

    Create misumi.h

---
+++ 2024_02/APM-2_code/Motor/ver1.x/ver1.2.1/misumi.h
@@ -0,0 +1,46 @@
+/*
+APM-2의 모터를 구동하기 위한 라이브러리 ver1.0
+*/
+
+#ifndef misumi_h
+#define misumi_h
+
+class misumi
+{
+    public:
+        misumi(int driverPUL, int driverDIR, long resolution);
+
+        void setSpeed(int delaySpeed);
+
+        void move_motor_angle();
+        void move_moter(long steps);
+        void set_angle(long angle);
+        
+        void revmotor();
+
+        //void test_function(int PR);
+        
+    private:
+        //motor pin
+        int driverPUL;
+        int driverDIR;
+        int driverENA;
+        int driverALM;
+
+        //motor speed
+        int delaySpeed;
+
+        //motor pulse/rev
+        long PR;
+
+        //motor save steps
+        long leftSteps;
+
+        //pre millis
+        unsigned long preMicros;
+
+        //motor diraction
+        bool setdir;
+};
+
+#endif
