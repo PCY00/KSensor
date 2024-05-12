a3f713e56d5c02daf9eb1623670a510fc9eb89bc
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-05-12 14:50:34 +0900

    Create misumi.cpp

---
+++ 2024_02/APM-2_code/Motor/ver1.x/ver1.2/misumi.cpp
@@ -0,0 +1,67 @@
+#include "misumi.h"
+#include <Arduino.h>
+
+misumi::misumi(int driverPUL, int driverDIR, long resolution) {
+    this->driverPUL = driverPUL;
+    this->driverDIR = driverDIR;
+    this->PR = resolution;
+
+    this->preMillis = 0;
+    this->leftSteps = resolution;
+
+    this->setdir = HIGH;
+
+    pinMode(driverPUL, OUTPUT);
+    pinMode(driverDIR, OUTPUT);
+
+    digitalWrite(driverDIR, setdir);
+
+    Serial.println("start");
+    
+}
+
+
+void misumi::setSpeed(int delaySpeed) {
+//101 ~ 16000
+    this->delaySpeed = delaySpeed;
+}
+
+void misumi::revmotor() {
+    this->leftSteps = this->PR;
+    setdir = !setdir;
+    digitalWrite(driverDIR, setdir);
+}
+
+void misumi::move_motor_angle(){
+    unsigned long currentMillis = micros();
+
+    if(currentMillis - this->preMillis >= this->delaySpeed){
+        this->preMillis = currentMillis;
+        Serial.println(preMillis);
+        if(this->leftSteps > 0){
+            this->leftSteps--;
+            move_moter(leftSteps % 2);
+        }
+    }
+}
+
+void misumi::move_moter(long steps){
+    Serial.println("come1");
+    switch(steps){
+        case 0:
+            digitalWrite(driverPUL, HIGH);
+            break;
+        case 1:
+            digitalWrite(driverPUL, LOW);
+            break;
+    }
+}
+/*
+void misumi::test_function(int PR) {
+    for (int i = 0; i < PR; i++) {
+        digitalWrite(driverPUL, HIGH);
+        delayMicroseconds(delaySpeed);
+        digitalWrite(driverPUL, LOW);
+        delayMicroseconds(delaySpeed);
+    }
+}*/
