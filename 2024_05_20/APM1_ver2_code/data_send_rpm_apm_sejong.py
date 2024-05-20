4fb6c9d12afb48d4b99ff33d9afa53ae3080548d
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-05-20 21:03:27 +0900

    Update data_send_rpm_apm_sejong.py

--- 2024_05_20/APM1_ver2_code/data_send_rpm_apm_sejong.py
+++ 2024_05_20/APM1_ver2_code/data_send_rpm_apm_sejong.py
@@ -32,6 +32,9 @@ brate0 = 9600
 brate1 = 9600
 brate2 = 9600
 
+#signal
+changetime = False
+
 # Initialize serial connections
 try:
     ser0 = Serial(port0, baudrate=brate0, timeout=None)
@@ -59,7 +62,7 @@ def fetch_data():
         return None
 
 def process_data():
-    global last_sent_data, pm_data_save, ser0, ser1, ser2, time_s, pm_data
+    global last_sent_data, pm_data_save, ser0, ser1, ser2, time_s, pm_data, changetime
     data = fetch_data()
     if data:
         for item in data.get('data', []):
@@ -71,121 +74,128 @@ def process_data():
                     time_s = meastime
                     pm_data_save = pm2_5_a
                     pm_data = pm_data_save
+                    
+                    changetime = True
                 
-                send_s = 'start'
-                send_S = send_s.encode('utf-8')
-                
-                if ser0:
-                    ser0.write(send_S)
-                if ser1:
-                    ser1.write(send_S)
-                if ser2:
-                    ser2.write(send_S)
-                
-                time.sleep(2)
-                return time_s, pm_data
+                    send_s = 'start'
+                    send_S = send_s.encode('utf-8')
+                    
+                    if ser0:
+                        ser0.write(send_S)
+                    if ser1:
+                        ser1.write(send_S)
+                    if ser2:
+                        ser2.write(send_S)
+                    
+                    time.sleep(2)
+                    return time_s, pm_data, changetime
     else:
         print("No data received")
-    return time_s, pm_data
+    
+    changetime = False
+    return time_s, pm_data, changetime
 
 while True:
-    url_sejong_uni = ""
+    time_s, pm_data, changetime = process_data()
     
-    time_s, pm_data = process_data()
+    if(changetime):
+        url_sejong_uni = ""
     
-    if ser0 and ser1 and ser2 and ser0.in_waiting and ser1.in_waiting and ser2.in_waiting:
-        try:
-            contect2 = ser2.readline().decode().strip()
-            contect0 = ser0.readline().decode().strip()
-            contect1 = ser1.readline().decode().strip()
-            
-            con0_1 = "{},{},{},{}".format(time_s, contect2, contect0, contect1)
-            print(con0_1)
-
-        except Exception as err:
-            print("Serial read error:", err)
-            continue
-        
-        con0_1 = con0_1.replace("\n", "")
-        data_list = con0_1.split(',')
-            
-        bottom, pm1 = data_list[2], data_list[3]
-            
-        if bottom == "0":
-            if pm1 == "1":
-                url_sejong_uni = url_sejong + "/param1"
-            elif pm1 == "2":
-                url_sejong_uni = url_sejong + "/param2"
-            elif pm1 == "3":
-                url_sejong_uni = url_sejong + "/param3"
-        elif bottom == "30":
-            if pm1 == "1":
-                url_sejong_uni = url_sejong + "/param4"
-            elif pm1 == "2":
-                url_sejong_uni = url_sejong + "/param5"
-            elif pm1 == "3":
-                url_sejong_uni = url_sejong + "/param6"
-        elif bottom == "60":
-            if pm1 == "1":
-                url_sejong_uni = url_sejong + "/param7"
-            elif pm1 == "2":
-                url_sejong_uni = url_sejong + "/param8"
-            elif pm1 == "3":
-                url_sejong_uni = url_sejong + "/param9"
-        elif bottom == "90":
-            if pm1 == "1":
-                url_sejong_uni = url_sejong + "/param10"
-            elif pm1 == "2":
-                url_sejong_uni = url_sejong + "/param11"
-            elif pm1 == "3":
-                url_sejong_uni = url_sejong + "/param12"
-                
-        print(url_sejong_uni)
-            
-        new_data_list = []
-        for item in data_list[:2] + data_list[6:]:
+        if ser0 and ser1 and ser2 and ser0.in_waiting and ser1.in_waiting and ser2.in_waiting:
             try:
-                new_data_list.append(float(item))
-            except ValueError:
-                new_data_list.append(item)
+                contect2 = ser2.readline().decode().strip()
+                contect0 = ser0.readline().decode().strip()
+                contect1 = ser1.readline().decode().strip()
+                
+                con0_1 = "{},{},{},{}".format(time_s, contect2, contect0, contect1)
+                print(con0_1)
 
-        con0_1_sejong_json = OrderedDict([
-            ("label", float(pm_data)),
-            ("data", new_data_list)
-        ])
-        con0_1_sejong_str = json.dumps(con0_1_sejong_json)
-        # print(con0_1_sejong_str)
-        
-        data_apm = json.dumps({
-            "m2m:cin": {
-                "con": con0_1
-            }
-        })
-        data_sejong = json.dumps({
-            "m2m:cin": {
-                "con": con0_1_sejong_str
-            }
-        })
-        # print(data_sejong)
-        
-        try:
-            r_apm = requests.post(apm_url, headers=headers_apm, data=data_apm)
-            r_apm.raise_for_status()
-        except requests.exceptions.RequestException as req_err:
-            print("APM Request error:", req_err)
-        except requests.exceptions.HTTPError as http_err:
-            print("APM HTTP error:", http_err)
-        except Exception as exc:
-            print('APM There was a problem: {}'.format(exc))
-        
-        try:
-            r_sejong = requests.post(url_sejong_uni, headers=headers_sejong, data=data_sejong)
-            r_sejong.raise_for_status()
-        except requests.exceptions.RequestException as req_err:
-            print("Sejong Request error:", req_err)
-        except requests.exceptions.HTTPError as http_err:
-            print("Sejong HTTP error:", http_err)
-        except Exception as exc:
-            print('Sejong There was a problem: {}'.format(exc))
-    
-    time.sleep(55)
+            except Exception as err:
+                print("Serial read error:", err)
+                continue
+            
+            con0_1 = con0_1.replace("\n", "")
+            data_list = con0_1.split(',')
+                
+            bottom, pm1 = data_list[2], data_list[3]
+                
+            if bottom == "0":
+                if pm1 == "1":
+                    url_sejong_uni = url_sejong + "/param1"
+                elif pm1 == "2":
+                    url_sejong_uni = url_sejong + "/param2"
+                elif pm1 == "3":
+                    url_sejong_uni = url_sejong + "/param3"
+            elif bottom == "30":
+                if pm1 == "1":
+                    url_sejong_uni = url_sejong + "/param4"
+                elif pm1 == "2":
+                    url_sejong_uni = url_sejong + "/param5"
+                elif pm1 == "3":
+                    url_sejong_uni = url_sejong + "/param6"
+            elif bottom == "60":
+                if pm1 == "1":
+                    url_sejong_uni = url_sejong + "/param7"
+                elif pm1 == "2":
+                    url_sejong_uni = url_sejong + "/param8"
+                elif pm1 == "3":
+                    url_sejong_uni = url_sejong + "/param9"
+            elif bottom == "90":
+                if pm1 == "1":
+                    url_sejong_uni = url_sejong + "/param10"
+                elif pm1 == "2":
+                    url_sejong_uni = url_sejong + "/param11"
+                elif pm1 == "3":
+                    url_sejong_uni = url_sejong + "/param12"
+                    
+            #print(url_sejong_uni)
+                
+            new_data_list = []
+            for item in data_list[:2] + data_list[6:]:
+                try:
+                    new_data_list.append(float(item))
+                except ValueError:
+                    new_data_list.append(item)
+
+            con0_1_sejong_json = OrderedDict([
+                ("label", float(pm_data)),
+                ("data", new_data_list)
+            ])
+            con0_1_sejong_str = json.dumps(con0_1_sejong_json)
+            # print(con0_1_sejong_str)
+            
+            data_apm = json.dumps({
+                "m2m:cin": {
+                    "con": con0_1
+                }
+            })
+            data_sejong = json.dumps({
+                "m2m:cin": {
+                    "con": con0_1_sejong_str
+                }
+            })
+            # print(data_sejong)
+            
+            try:
+                r_apm = requests.post(apm_url, headers=headers_apm, data=data_apm)
+                r_apm.raise_for_status()
+            except requests.exceptions.RequestException as req_err:
+                print("APM Request error:", req_err)
+            except requests.exceptions.HTTPError as http_err:
+                print("APM HTTP error:", http_err)
+            except Exception as exc:
+                print('APM There was a problem: {}'.format(exc))
+            
+            try:
+                r_sejong = requests.post(url_sejong_uni, headers=headers_sejong, data=data_sejong)
+                r_sejong.raise_for_status()
+            except requests.exceptions.RequestException as req_err:
+                print("Sejong Request error:", req_err)
+            except requests.exceptions.HTTPError as http_err:
+                print("Sejong HTTP error:", http_err)
+            except Exception as exc:
+                print('Sejong There was a problem: {}'.format(exc))
+                
+            changetime = False
+            
+        time.sleep(55)
