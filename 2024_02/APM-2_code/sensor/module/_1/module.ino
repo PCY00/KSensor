428eec9a7c5cb76cac6d737aa8ff71dc875f52ec
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-07-04 16:33:56 +0900

    Update module.ino

--- 2024_02/APM-2_code/sensor/module/_1/module.ino
+++ 2024_02/APM-2_code/sensor/module/_1/module.ino
@@ -1,5 +1,9 @@
 #include <PMS.h>                //pms7003
 
+#define Speed_1 40
+#define Speed_2 55
+#define Speed_3 70
+
 PMS pms1(Serial3);
 PMS::DATA data1;
 
@@ -10,10 +14,17 @@ PMS pms3(Serial1);
 PMS::DATA data3;
 
 uint16_t d1, d2, d3;
-const int fanPin = 9; // 팬을 제어하는 핀
+const int fanPin1 = 8;
+const int fanPin2 = 9;
+const int fanPin3 = 10;
+
 const int maxSpeed = 255; // 최대 속도 (0-255)
-int fanSpeed = 0; // 현재 팬 속도
-String con_data = "";
+
+int fanSpeed1 = 0; // 현재 팬 속도
+int fanSpeed2 = 0; // 현재 팬 속도
+int fanSpeed3 = 0; // 현재 팬 속도
+
+String con_data = "0,0,0";
 
 bool startReceived = false;
 
@@ -24,7 +35,13 @@ void setup() {
   Serial.begin(9600);
   delay(1000);
 
-  pinMode(fanPin, OUTPUT);
+  pinMode(fanPin1, OUTPUT);
+  pinMode(fanPin2, OUTPUT);
+  pinMode(fanPin3, OUTPUT);
+  
+  analogWrite(fanPin1, fanSpeed1);
+  analogWrite(fanPin2, fanSpeed2);
+  analogWrite(fanPin3, fanSpeed3);
   
   pms1.passiveMode();
   pms2.passiveMode();
@@ -33,22 +50,75 @@ void setup() {
   
 }
 
+//ex) M1_P1_70
+
 void loop() {
   //젯슨에서 값 받는거 대기
   while (!startReceived) {
   // start 신호 받기
     if (Serial.available() > 0) {
       String input = Serial.readStringUntil('\n');
-      if (input.startsWith("p_")) {
-        fanSpeed = input.substring(2).toInt(); // 문자열에서 숫자 부분 추출
-        
-        analogWrite(fanPin, fanSpeed);
-        
-        con_data = String(fanSpeed);
-        Serial.println(con_data);
+      if (input.startsWith("M1_P")) {
+        //컨테이너 안 모듈 넘버
+        if(input.substring(4,5).toInt() == 1){
+          fanSpeed1 = input.substring(6).toInt();
+          switch(fanSpeed1){
+            case Speed_1:
+              analogWrite(fanPin1, Speed_1);
+              break;
+            case Speed_2:
+              analogWrite(fanPin1, Speed_2);
+              break;
+            case Speed_3:
+              analogWrite(fanPin1, Speed_3);
+              break;
+            default:
+              //Serial.println("error");
+              break;
+          }
+        }else if(input.substring(4,5).toInt() == 2){
+          fanSpeed2 = input.substring(6).toInt();
+          switch(fanSpeed2){
+            case Speed_1:
+              analogWrite(fanPin2, Speed_1);
+              break;
+            case Speed_2:
+              analogWrite(fanPin2, Speed_2);
+              break;
+            case Speed_3:
+              analogWrite(fanPin2, Speed_3);
+              break;
+            default:
+              //Serial.println("error");
+              break;
+          }
+        }else if(input.substring(4,5).toInt() == 3){
+          fanSpeed3 = input.substring(6).toInt();
+          switch(fanSpeed3){
+            case Speed_1:
+              analogWrite(fanPin3, Speed_1);
+              break;
+            case Speed_2:
+              analogWrite(fanPin3, Speed_2);
+              break;
+            case Speed_3:
+              analogWrite(fanPin3, Speed_3);
+              break;
+            default:
+              //Serial.println("error");
+              break;
+          } 
+        }else{
+          //Serial.println("module number error");
+        }
+      }else if(input == "start") {
         startReceived = true;
+      }else{
+        //Serial.println("error");
       }
     }
+    con_data = String(fanSpeed1)+ "," + String(fanSpeed2)+ "," + String(fanSpeed3);
+    //Serial.println(con_data);
   }
   if (startReceived == true) {
 
@@ -65,14 +135,16 @@ void loop() {
     if(pms3.readUntil(data3)){
       d3 = data3.PM_AE_UG_2_5;
     }
-
+    
+    Serial.print(con_data);
+    Serial.print(",");
     Serial.print(d1);
     Serial.print(",");
     Serial.print(d2);
     Serial.print(",");
     Serial.print(d3);
     Serial.print(",");
-
+    
     startReceived = false;
   }
 }
