b15568b2b0cc8319f515de0b92e5736b2e09c6af
PCY00 <46348321+PCY00@users.noreply.github.com>
2024-08-17 19:49:07 +0900

    Update apm2_data_Pre_processing.py

--- 2024_07_30/2024_02/APM-2_code/apm2_data_Pre_processing.py
+++ 2024_07_30/2024_02/APM-2_code/apm2_data_Pre_processing.py
@@ -1,185 +1,232 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+import socket
 import serial
 import requests
 import time
 import json
+import signal
+import sys
+from threading import Lock
 
-# Module1,2,3
-port0 = '/dev/ttyACM0'
-port1 = '/dev/ttyACM1'
-port2 = '/dev/ttyACM2'
-# Sensor
-port3 = '/dev/ttyACM3'
-# NPM
-port4 = '/dev/ttyACM4'
-
+# 기존 URL 및 헤더 설정
 apm2_url = 'http://114.71.220.59:2021/Mobius/Ksensor_ubicomp_2/data'
-rpm_url = "http://211.226.18.66:5002/iot/v1/microdust/measure-last"
-
-brate0 = 9600
-brate1 = 9600
-brate2 = 9600
-brate3 = 9600
-brate4 = 9600
+data_send_url = 'http://114.71.220.59:2021/Mobius/Ksensor_ubicomp_2/motor'
+rpm_url = "http://114.71.220.59:2021/Mobius/Ksensor_ubicomp_2/RPMData"
+FanSpeed_url = "http://114.71.220.59:2021/Mobius/Ksensor_ubicomp_2/FanSpeed"
 
 apm2_headers = {
     'Accept': 'application/json',
     'X-M2M-RI': '12345',
-    'X-M2M-Origin': 'SKsensor_ubicomp_2',
+    'X-M2M-Origin': 'Sgw',
     'Content-Type': 'application/vnd.onem2m-res+json; ty=4'
 }
 
+# 시리얼 포트 및 보드레이트 설정
+serial_port_0 = '/dev/ttyACM0'
+serial_port_1 = '/dev/ttyACM1'
+serial_port_2 = '/dev/ttyACM2'
+serial_port_3 = '/dev/ttyACM3'
+serial_port_4 = '/dev/ttyACM4'
+
+baudrate = 9600
+
+# 시리얼 연결 초기화
 try:
-    ser0 = serial.Serial(port0, baudrate=brate0, timeout=None)
-    ser1 = serial.Serial(port1, baudrate=brate1, timeout=None)
-    ser2 = serial.Serial(port2, baudrate=brate2, timeout=None)
-    ser3 = serial.Serial(port3, baudrate=brate3, timeout=None)
-    ser4 = serial.Serial(port4, baudrate=brate4, timeout=None)
-    print('%s and %s and %s and %s and %s' % (ser0.name, ser1.name,ser2.name, ser3.name,ser4.name))
+    ser0 = serial.Serial(serial_port_0, baudrate=baudrate, timeout=1)
+    ser1 = serial.Serial(serial_port_1, baudrate=baudrate, timeout=1)
+    ser2 = serial.Serial(serial_port_2, baudrate=baudrate, timeout=1)
+    ser3 = serial.Serial(serial_port_3, baudrate=baudrate, timeout=1)
+    ser4 = serial.Serial(serial_port_4, baudrate=baudrate, timeout=1)
+    print(f'Connected to {ser0.name},{ser1.name},{ser2.name},{ser3.name},{ser4.name}')
 except Exception as err:
-    print("Serial err:", err)
+    print(f"Serial error: {err}")
+    sys.exit(1)
     
-# Global variables
-time_s, pm_data = "", ""
-last_sent_data, pm_data_save = "", ""
+def clear_serial_buffer():
+    ser0.reset_input_buffer()
+    ser1.reset_input_buffer()
+    ser2.reset_input_buffer()
+    ser3.reset_input_buffer()
+    ser4.reset_input_buffer()
+    print("Serial buffers cleared")
 
-def fetch_data():
+first = 0
+# 초기화 시 메시지 전송
+if first == 0:
+    ser0.write(b'0')
+    ser1.write(b'0')
+    ser2.write(b'0')
+    ser3.write(b'0')
+    ser4.write(b'0')
+    first = 1
+    clear_serial_buffer()
+    print("loading....")
+    time.sleep(5)
+
+# 서버 설정
+port = 65432
+
+# 전역 변수 초기화
+time_s, pm_data, pm_data_a = "", "", ""
+running = True
+
+# 임계영역 보호를 위한 Lock 생성
+lock = Lock()
+
+def read_serial_data():
     try:
-        response = requests.get(rpm_url)
-        response.raise_for_status()
-        return response.json()
-    except requests.HTTPError as http_err:
-        print("HTTP error occurred: {} - {}".format(http_err, response.status_code))
-        return None
-    except Exception as e:
-        print("Failed to fetch data: {}".format(e))
-        return None
+        data = ser0.readline().decode('utf-8').strip()
+        data1 = ser1.readline().decode('utf-8').strip()
+        data2 = ser2.readline().decode('utf-8').strip()
+        data3 = ser3.readline().decode('utf-8').strip()
+        data4 = ser4.readline().decode('utf-8').strip()
+        if data == " , " || data1 == " , " || data2 == " , " || data3 == " , " || data4 == " , ":
+            return "error"
+        return data
+    except Exception as read_err:
+        print(f"Serial read error: {read_err}")
+        return "error"
 
-def process_data():
-    global last_sent_data, pm_data_save, ser0, ser1, ser2, time_s, pm_data
-    data = fetch_data()
-    if data:
-        for item in data.get('data', []):
-            if item.get('deviceId') == '0017B2FFFE50087D':
-                meastime = item.get('meastime', '')
-                pm2_5_a = item.get('pm2.5_a', '')
-                if meastime != last_sent_data or not last_sent_data:
-                    last_sent_data = meastime
-                    time_s = meastime
-                    pm_data_save = pm2_5_a
-                    pm_data = pm_data_save
-                send_1 = "start_m1"
-                send_2 = "start_m2"
-                send_3 = "start_m3"
-                send_4 = "start_sensor"
-                send_5 = "start_npm"
-                    
-                send_S1 = send_1.encode('utf-8')
-                send_S2 = send_2.encode('utf-8')
-                send_S3 = send_3.encode('utf-8')
-                send_S4 = send_4.encode('utf-8')
-                send_S5 = send_5.encode('utf-8')
-                
-                print(send_S1);
-                print(send_S2);
-                print(send_S3);
-                print(send_S4);
-                print(send_S5);
-                
-                if ser0:
-                    ser0.write(send_S1)
-                    print("1");
-                if ser1:
-                    ser1.write(send_S2)
-                    print("2");
-                if ser2:
-                    ser2.write(send_S3)
-                    print("3");
-                if ser3:
-                    ser3.write(send_S4)
-                    print("4");
-                if ser4:
-                    ser4.write(send_S5)
-                    print("5");
-                
-                time.sleep(2)
-                return time_s, pm_data
-    else:
-        print("No data received")
-    return time_s, pm_data
+def send_apm_data(data, url):
+    data_apm = json.dumps({
+        "m2m:cin": {
+            "con": data
+        }
+    })
 
-while True:
-    time_s, pm_data = process_data()
-    print(time_s);
-    print(pm_data);
-    
-    
-    if ser0.in_waiting and ser1.in_waiting and ser2.in_waiting and ser3.in_waiting and ser4.in_waiting:
-        try:
-            contect0 = ser0.readline().decode('utf-8').strip()
-            contect1 = ser1.readline().decode('utf-8').strip()
-            contect2 = ser2.readline().decode('utf-8').strip()
-            contect3 = ser3.readline().decode('utf-8').strip()
-            contect4 = ser4.readline().decode('utf-8').strip()
-            
-            con0_1 = "{},{},{},{},{},{},{},{}".format(time_s, contect0, contect1, contect2, contect3, contect4, pm_data, pm_data)
-            
-            # 데이터 정리
-            data_list = con0_1.split(',')
-            
-            # 원래 데이터 순서 (0부터 시작하는 인덱스)
-            original_order = [
-                "datetime", "pwm1_1", "pwm1_2", "pwm1_3", "pm1_1", "pm1_2", "pm1_3", 
-                "pwm2_1", "pwm2_2", "pwm2_3", "pm2_1", "pm2_2", "pm2_3", 
-                "pwm3_1", "pwm3_2", "pwm3_3", "pm3_1", "pm3_2", "pm3_3", 
-                "temp", "humi", "o3", "co", "no2", "so2",
-                "wind_d", "wind_s", "npm", "rpm보정 전", "rpm 보정후"
-            ]
-            
-            # 원하는 순서 (0부터 시작하는 인덱스)
-            desired_order = [
-                "datetime", "rpm보정 전", "rpm 보정후", 
-                "pwm1_1", "pwm1_2", "pwm1_3", 
-                "pwm2_1", "pwm2_2", "pwm2_3", 
-                "pwm3_1", "pwm3_2", "pwm3_3", 
-                "Bottom1", "Bottom2", "Bottom3", 
-                "npm", "pm1_1", "pm1_2", "pm1_3", 
-                "pm2_1", "pm2_2", "pm2_3", 
-                "pm3_1", "pm3_2", "pm3_3", 
-                "temp", "humi", "o3", "co", 
-                "no2", "so2", "wind_d", "wind_s"
-            ]
-            
-            # 원래 순서에서 원하는 순서로 데이터 재배치
-            index_map = {original_order[i]: i for i in range(len(original_order))}
-            reordered_data = [data_list[index_map[col]] for col in desired_order]
+    try:
+        r_apm2 = requests.post(url, headers=apm2_headers, data=data_apm)
+        r_apm2.raise_for_status()
+    except requests.exceptions.RequestException as req_err:
+        print("APM request error:", req_err)
+    except requests.exceptions.HTTPError as http_err:
+        print("APM HTTP error:", http_err)
+    except Exception as exc:
+        print(f'APM problem occurred: {exc}')
 
-            # 재조합된 데이터 출력
-            con0_1 = ','.join(reordered_data)
-            print(con0_1)
+def process_data(data):
+    global time_s, pm_data, pm_data_a
 
-        except Exception as err:
-            print("Serial read error:", err)
-            continue
+    with lock:  # 임계영역 시작
+        if data.startswith('M'):
+            clear_serial_buffer()
+            if data[1] == '1':
+                ser0.write(data.encode('utf-8'))
+                time.sleep(5)
+            elif data[1] == '2':
+                ser1.write(data.encode('utf-8'))
+                time.sleep(5)
+            elif data[1] == '3':
+                ser2.write(data.encode('utf-8'))
+                time.sleep(5)
+
+            FanSpeed_data = read_serial_data()
+            print(f"Received RPM data: {FanSpeed_data}")
+            send_apm_data(FanSpeed_data, FanSpeed_url)
+
+        elif data.startswith('2'):
+            rpm_data_list = data.split(',')
+            time_s, pm_data, pm_data_a = rpm_data_list[0], rpm_data_list[5], rpm_data_list[6]
         
-        con0_1 = con0_1.replace("\n", "")
+            print(f"Data starts with '2': {time_s},{pm_data},{pm_data_a}\n")
+            clear_serial_buffer()
+            print(f"sending start")
+            ser0.write(b'start')
+            ser1.write(b'start')
+            ser2.write(b'start')
+            ser3.write(b'start')
+            ser4.write(b'start')
+            time.sleep(5)
         
-        data_apm = json.dumps({
-            "m2m:cin": {
-                "con": con0_1
-            }
-        })
-        
-        try:
-            r_apm2 = requests.post(apm2_url, headers=apm2_headers, data=data_apm)
-            r_apm2.raise_for_status()
-        except requests.exceptions.RequestException as req_err:
-            print("APM Request error:", req_err)
-        except requests.exceptions.HTTPError as http_err:
-            print("APM HTTP error:", http_err)
-        except Exception as exc:
-            print('APM There was a problem: {}'.format(exc))
-    
-    time.sleep(55)
+            con0_1 = read_serial_data()
+            if "error" in con0_1:
+                clear_serial_buffer()
+                print("Error detected, resending 'start' signal")
+                ser0.write("start".encode('utf-8'))
+                ser1.write("start".encode('utf-8'))
+                ser2.write("start".encode('utf-8'))
+                ser3.write("start".encode('utf-8'))
+                ser4.write("start".encode('utf-8'))
+                time.sleep(5)
+            else:
+                if time_s and pm_data and pm_data_a:
+                    combined_data = "{},{},{},{}".format(time_s, con0_1, pm_data, pm_data_a)
+                    print(f"{combined_data}")
 
+                    data_list = combined_data.split(',')
+                    original_order = [
+                        "datetime", "pwm1_1", "pwm1_2", "pwm1_3", 
+                        "pm1_1", "pm1_2", "pm1_3", 
+                        "rpm before correction", "rpm after correction"
+                    ]
+
+                    desired_order = [
+                        "datetime", "rpm before correction", "rpm after correction",
+                        "pwm1_1", "pwm1_2", "pwm1_3",
+                        "pm1_1", "pm1_2", "pm1_3",
+                    ]
+
+                    if len(data_list) != len(original_order):
+                        clear_serial_buffer()
+                        print("Data length mismatch: Retrying.")
+                        ser0.write("start".encode('utf-8'))
+                        ser1.write("start".encode('utf-8'))
+                        ser2.write("start".encode('utf-8'))
+                        ser3.write("start".encode('utf-8'))
+                        ser4.write("start".encode('utf-8'))
+                        time.sleep(5)
+
+                    index_map = {original_order[i]: i for i in range(len(original_order))}
+                    reordered_data = [data_list[index_map[col]] for col in desired_order]
+
+                    combined_data = ','.join(reordered_data)
+
+                    print(f"Reordered data: {combined_data}")
+                    send_apm_data(combined_data, apm2_url)
+                else:
+                    print("Global variables are not yet initialized.")        
+
+def server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('localhost', port))
+        s.listen()
+        print(f"Listening on port {port}...")
+
+        while running:
+            conn, addr = s.accept()
+            with conn:
+                print(f'Connected to {addr}')
+                while running:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    data = data.decode('utf-8').strip()
+                    process_data(data)
+
+def signal_handler(sig, frame):
+    global running
+    print('Safely shutting down...')
+    running = False
+    ser0.close()
+    ser1.close()
+    ser2.close()
+    ser3.close()
+    ser4.close()
+    
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# 서버 실행
+try:
+    server()
+except Exception as e:
+    print(f"Server error: {e}")
+finally:
+    ser0.close()
+    ser1.close()
+    ser2.close()
+    ser3.close()
+    ser4.close()
+    print("Serial connection closed.")
